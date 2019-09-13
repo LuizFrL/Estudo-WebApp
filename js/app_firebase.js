32bd85df4e28342f26488690238c183dedd3ed38
Luiz Fernando <luizfernandorole@gmail.com>
2019-09-12 22:37:44 -0300

    Falar com fermino sobre, não estou conseguindo fazer a aitenticação e estou irritado

---
+++ js/app_firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAJzqVc5O6HOtpFgkU901zZnG9YSLTMgmU",
+  authDomain: "aplicativo-web-estudo.firebaseapp.com",
+  databaseURL: "https://aplicativo-web-estudo.firebaseio.com",
+  projectId: "aplicativo-web-estudo",
+  storageBucket: "",
+  messagingSenderId: "857250259597",
+  appId: "1:857250259597:web:68044ab90f586530b938c4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
